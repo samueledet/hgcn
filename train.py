d31d717844c03594791f5ac072d2ce117ac93972
Samuel Edet <samueledet@aims.ac.za>
2020-01-05 11:45:10 +0100

    Update train.py

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ def train(args):
     logging.info("Using seed {}.".format(args.seed))
 
     # Load data
-    DATAPATH = '/home/samuele.edet/hgcn/data'
+    DATAPATH = '/storage/samuele.edet/hgcn/data'
     data = load_data(args, os.path.join(DATAPATH, args.dataset))
     args.n_nodes, args.feat_dim = data['features'].shape
     if args.task == 'nc':
